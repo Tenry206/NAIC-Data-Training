dab5a724820dafcaee18dd698b6de97eb104f2c5
Tenry206 <tenrytan55@gmail.com>
2025-05-13 17:44:54 +0800

    TT

--- augmentedData.py
+++ augmentedData.py
@@ -4,9 +4,10 @@ import numpy as np
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras import layers
 from PIL import Image
+import tensorflow_addons as tfa
 
 # --- Config ---
-source_dir = "C:/Users/User/OneDrive - University of Nottingham Malaysia/Documents/National ai Competition/DataSet"
+source_dir = "C:/Users/User/OneDrive - University of Nottingham Malaysia/Documents/National ai Competition/AugTest"
 target_dir = "C:/Users/User/OneDrive - University of Nottingham Malaysia/Documents/National ai Competition/AugmentedData"
 
 img_size = (180, 180)
@@ -21,9 +22,30 @@ data_augmentation = tf.keras.Sequential([
     layers.RandomCrop(height=img_size[0], width=img_size[1]),
     layers.Lambda(lambda x: tf.image.random_brightness(x, max_delta=0.3)),
     layers.Lambda(lambda x: tf.image.random_saturation(x, lower=0.7, upper=1.3)),
-    layers.Lambda(lambda x: x + tf.random.uniform(tf.shape(x), minval=0, maxval=0.1)),  # Add noise
+    layers.Lambda(lambda x: tf.image.rot90(x, k=tf.random.uniform(shape=[], minval=0, maxval=4, dtype=tf.int32))),  # Random 90° rotation
+    layers.Lambda(lambda x: tfa.image.gaussian_filter2d(x, filter_shape=(3, 3), sigma=1.0)),  # Gaussian blur with 1px radius
+    layers.Lambda(lambda x: add_sparse_noise(x, noise_ratio=0.0065)),  # Add noise to 0.65% of pixels
     layers.Lambda(lambda x: tf.clip_by_value(x, 0.0, 1.0)),
 ])
+
+def add_sparse_noise(image, noise_ratio=0.0065):
+    """
+    Adds random noise to a specified percentage of pixels in the image.
+    Args:
+        image: 3D tensor of shape [height, width, channels], with pixel values in [0, 1].
+        noise_ratio: Float, the fraction of pixels to which noise will be added.
+    Returns:
+        Augmented image with sparse noise.
+    """
+    # Create a mask for pixels to be noised
+    mask = tf.cast(tf.random.uniform(tf.shape(image)) < noise_ratio, tf.float32)
+    # Generate random noise
+    noise = tf.random.uniform(tf.shape(image), minval=0.0, maxval=1.0)
+    # Apply noise to selected pixels
+    noisy_image = image + mask * noise
+    return noisy_image
+
+
 
 # --- Load Dataset ---
 dataset = image_dataset_from_directory(
